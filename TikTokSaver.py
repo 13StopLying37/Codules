51ced21c432a0b72dc4506f7c7c8af4d727958e2
13StopLying37 <75055573+13StopLying37@users.noreply.github.com>
2022-01-25 13:24:54 +0200

    Upload file

---
+++ TikTokSaver.py
@@ -0,0 +1,20 @@
+﻿from .. import loader, utils
+
+
+def register(cb):
+    cb(TikTokMod())
+
+class TikTokMod(loader.Module):
+    """Качаем видео без ебаной Рекламы Тик Тока"""
+    strings = {'name': 'TikTok no WaterMark'}
+
+    async def тикcmd(self, message):
+        """Кидает тебе в ебало видео с Хуйтока без Варенье марки"""
+        await utils.answer(message, 'Погоди...')
+        args = utils.get_args_raw(message)
+        if not args:
+            await utils.answer(message, "Ссылку мне дай")
+            return
+        r = await message.client.inline_query('tikdobot', args)
+        await message.client.send_file(message.to_id, r[1].result.content.url)
+        await message.delete()
